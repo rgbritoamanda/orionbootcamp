c3db3262156c0bf4c07232429550171eec99946e
rgbritoamanda <amandaramires.brito@gmail.com>
2024-10-03 20:16:58 -0300

    Remoção require e readline dos arquivos em javascript

--- exercicios/ex001.js
+++ exercicios/ex001.js
@@ -1,21 +1,13 @@
-var readline = require('readline');
 function contarVogais(palavra) {
-    var contador = 0;
-    var vogais = "aeiouAEIOU";
-    for (var _i = 0, palavra_1 = palavra; _i < palavra_1.length; _i++) {
-        var letra = palavra_1[_i];
-        if (vogais.includes(letra)) {
-            contador++;
-        }
-    }
-    return contador;
+  var contador = 0;
+  var vogais = "aeiouAEIOU";
+  for (var _i = 0, palavra_1 = palavra; _i < palavra_1.length; _i++) {
+      var letra = palavra_1[_i];
+      if (vogais.includes(letra)) {
+          contador++;
+      }
+  }
+  return contador;
 }
+
 console.log("Quantidade de vogais em \"programacao\":", contarVogais("programacao"));
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('Digite uma palavra: ', function (palavraInput) {
-    console.log("Quantiade de vogais em \"".concat(palavraInput, "\":"), contarVogais(palavraInput));
-    rl.close();
-});
